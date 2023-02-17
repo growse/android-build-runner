04902c3f0a7621316c1be2283b5d250daf7cf889
Andrew Rowson <github@growse.com>
2023-02-17 11:40:16 +0000

    Patch sdkmanager to force http

--- set-gradle-properties-entrypoint.sh
+++ set-gradle-properties-entrypoint.sh
@@ -2,4 +2,8 @@
 
 printf '%s\n' "scan.uploadInBackground=false" "org.gradle.configureondemand=true" > /home/runner/.gradle/gradle.properties
 
+# Patch sdkmanager to force http
+
+find /home/runner/android-sdk -type f -name sdkmanager -exec sed -i 's/exec "\$JAVACMD" "\$@"$/exec "$JAVACMD" "$@" --no_https/' {} \;
+
 /usr/bin/entrypoint.sh
