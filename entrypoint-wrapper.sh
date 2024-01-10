200d5df06f8c3dcdea217daecbdf343b5e0ff4c4
Andrew Rowson <github@growse.com>
2024-01-10 10:29:20 +0000

    Disable max-workers

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-printf '%s\n' "scan.uploadInBackground=false" "org.gradle.console=plain" "org.gradle.workers.max=1" "org.gradle.daemon=false" "org.gradle.configuration-cache=true" "org.gradle.parallel=true" >/home/runner/.gradle/gradle.properties
+printf '%s\n' "scan.uploadInBackground=false" "org.gradle.console=plain" "org.gradle.daemon=false" "org.gradle.configuration-cache=true" "org.gradle.parallel=true" >/home/runner/.gradle/gradle.properties
 
 sudo rsync -azr /bootstrap/android-sdk/ /android-sdk/
 
