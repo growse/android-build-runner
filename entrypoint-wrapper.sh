0ebbe2b76885dd58d53b04f07850dfa4d4e711f7
Andrew Rowson <github@growse.com>
2024-01-10 11:28:05 +0000

    Gradle properties file is persistent, so check before adding each property

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 sudo rsync -azr /bootstrap/android-sdk/ /android-sdk/
 sudo chown -R runner /android-sdk
@@ -7,7 +7,10 @@ sudo chown -R runner /home/runner/.gradle
 # patch /dev/kvm permissions, because screw POSIX
 sudo chmod 666 /dev/kvm
 
-printf '%s\n' "scan.uploadInBackground=false" "org.gradle.console=plain" "org.gradle.daemon=false" "org.gradle.configuration-cache=true" "org.gradle.parallel=true" >/home/runner/.gradle/gradle.properties
+gradle_properties=("scan.uploadInBackground=false" "org.gradle.console=plain" "org.gradle.daemon=false" "org.gradle.configuration-cache=true" "org.gradle.parallel=true")
+for gradle_property in "${gradle_properties[@]}" ; do
+  grep -qxF "$gradle_property" /home/runner/.gradle/gradle.properties || echo "$gradle_property" >>/home/runner/.gradle/gradle.properties
+done
 
 rm -rf /home/runner/.android && mkdir -p /android-sdk/user_home && ln -s /android-sdk/user_home/ /home/runner/.android
 
