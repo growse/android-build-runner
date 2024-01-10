1e2603b66dee4c70f624bbadb9513b227273f79e
Andrew Rowson <github@growse.com>
2024-01-10 11:18:50 +0000

    Gradle.properties should generate after chown

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-printf '%s\n' "scan.uploadInBackground=false" "org.gradle.console=plain" "org.gradle.daemon=false" "org.gradle.configuration-cache=true" "org.gradle.parallel=true" >/home/runner/.gradle/gradle.properties
-
 sudo rsync -azr /bootstrap/android-sdk/ /android-sdk/
-
 sudo chown -R runner /android-sdk
 sudo chown -R runner /home/runner/.gradle
 
 # patch /dev/kvm permissions, because screw POSIX
 sudo chmod 666 /dev/kvm
+
+printf '%s\n' "scan.uploadInBackground=false" "org.gradle.console=plain" "org.gradle.daemon=false" "org.gradle.configuration-cache=true" "org.gradle.parallel=true" >/home/runner/.gradle/gradle.properties
 
 rm -rf /home/runner/.android && mkdir -p /android-sdk/user_home && ln -s /android-sdk/user_home/ /home/runner/.android
 
