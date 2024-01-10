0df902d7195bf38e0d72b0dc5db31f273b5c876e
Andrew Rowson <github@growse.com>
2024-01-10 11:33:56 +0000

    Gradle.properties should exist before we try and write to it

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -7,6 +7,7 @@ sudo chown -R runner /home/runner/.gradle
 # patch /dev/kvm permissions, because screw POSIX
 sudo chmod 666 /dev/kvm
 
+touch /home/runner/.gradle/gradle.properties
 gradle_properties=("scan.uploadInBackground=false" "org.gradle.console=plain" "org.gradle.daemon=false" "org.gradle.configuration-cache=true" "org.gradle.parallel=true")
 for gradle_property in "${gradle_properties[@]}" ; do
   grep -qxF "$gradle_property" /home/runner/.gradle/gradle.properties || echo "$gradle_property" >>/home/runner/.gradle/gradle.properties
