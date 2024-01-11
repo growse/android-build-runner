8a9d2716b4bd2abddd4191e9dbc014958eecbfa5
Andrew Rowson <github@growse.com>
2024-01-11 09:51:21 +0000

    Make sure gradle caching directory exists on startup

--- entrypoint-wrapper.sh
+++ entrypoint-wrapper.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Copy the container's Android SDK into the persistent storage
 sudo rsync -azr /bootstrap/android-sdk/ /android-sdk/
 sudo chown -R runner /android-sdk
 sudo chown -R runner /home/runner/.gradle
@@ -7,14 +8,20 @@ sudo chown -R runner /home/runner/.gradle
 # patch /dev/kvm permissions, because screw POSIX
 sudo chmod 666 /dev/kvm
 
+# Configure the build-specific gradle properties
 touch /home/runner/.gradle/gradle.properties
 gradle_properties=("scan.uploadInBackground=false" "org.gradle.console=plain" "org.gradle.daemon=false" "org.gradle.configuration-cache=false" "org.gradle.parallel=true" "org.gradle.caching=true")
 for gradle_property in "${gradle_properties[@]}" ; do
   grep -qxF "$gradle_property" /home/runner/.gradle/gradle.properties || echo "$gradle_property" >>/home/runner/.gradle/gradle.properties
 done
 
+# Touch the gradle caching directory to stop the gradle-build-action from pulling / pushing it up to Github.
+mkdir -p /home/runner/.gradle/caches
+
+# Configure gradle to use the on-site remote gradle cache
 mkdir -p /home/runner/.gradle/init.d/ && ln -s /home/runner/remoteCache.init.gradle.kts /home/runner/.gradle/init.d/remoteCache.init.gradle.kts
 
+# Put the Android AVD store onto persistent storage
 rm -rf /home/runner/.android && mkdir -p /android-sdk/user_home && ln -s /android-sdk/user_home/ /home/runner/.android
 
 /usr/bin/entrypoint.sh
